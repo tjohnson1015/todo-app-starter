fcf4fc1ac47fafa0c42fc8285274a9003f8fa1c1
Lachlan Heywood <lachieh@users.noreply.github.com>
2021-12-03 15:37:28 -0500

    add starter code

---
+++ data.js
@@ -0,0 +1,32 @@
+const todoItems = [
+  {
+    id: 1,
+    title: "Learn HTML",
+    completed: true
+  },
+  {
+    id: 2,
+    title: "Learn CSS",
+    completed: true
+  },
+  {
+    id: 3,
+    title: "Learn JS",
+    completed: true
+  },
+  {
+    id: 4,
+    title: "Learn React",
+    completed: false
+  },
+  {
+    id: 5,
+    title: "Learn Node",
+    completed: false
+  },
+  {
+    id: 6,
+    title: "Learn MongoDB",
+    completed: false
+  },
+];
