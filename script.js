b233d13863a3e06e1a13101748d309300ecae81f
Lachlan Heywood <lachieh@users.noreply.github.com>
2022-04-26 12:49:06 -0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// You can access the 'data' variable from the 'data.js' file, so long as 'data.js'
+// comes before 'script.js' in the HTML document
+console.log(data)
+
+// Use the 'data' variable to create the appropriate elements and append them onto
+// the page.
