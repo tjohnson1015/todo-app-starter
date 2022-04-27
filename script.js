23ac5761b6e16a09032aaa63aba65b7f2e243ad4
Tyler Johnson <tylerjohnson1015@yahoo.com>
2022-04-27 09:09:05 -0400

    initial commit for todo app HW

--- script.js
+++ script.js
@@ -4,3 +4,30 @@ console.log(todoItems)
 
 // Use the 'todoItems' variable to create the appropriate elements and append them onto
 // the page.
+
+const incompleteItems = document.querySelector('#incomplete-items')
+const completeItems = document.querySelector('#complete-items')
+
+for (let item of todoItems) {
+    const listItem = document.createElement('li')
+    listItem.innerText = item.title
+    listItem.classList.add('list-item')
+    if (item.completed === true) {
+        completeItems.appendChild(listItem)
+    }
+    else {
+        incompleteItems.appendChild(listItem)
+    }
+}
+
+
+var triggerTabList = [].slice.call(document.querySelectorAll('list-item'))
+triggerTabList.forEach(function (triggerEl) {
+var tabTrigger = new bootstrap.Tab(triggerEl)
+
+triggerEl.addEventListener('click', function (event) {
+    event.preventDefault()
+    tabTrigger.show()
+})
+})
+
